722e5c420fd0fe0ded1d247fe2662ef6da84a7de
Muhammad Ali Khalid <muhammadalikhalid101@gmail.com>
2024-05-31 17:08:36 +0500

    "Remove unnecessary code"

--- main.cpp
+++ main.cpp
@@ -2,11 +2,5 @@
 #include "Dynamic1DArray.h"
 
 int main() {
-    Dynamic1DArray <int> obj(-5);
-    obj.fillArray();
-    obj.displayArray();
-    obj.resizeArray(6);
-    obj.displayArray();
-    obj.deleteArray();
     return 0;
 }
